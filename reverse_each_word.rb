261727eb9b1629aafe95819d3f11e87a3e3964ed
John Plantamura <plantamurajk@gmail.com>
2019-08-15 13:54:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   sentence_array = sentence.split( " ")
   print sentence_array
   word_chars = sentence_array.map {|word| word.split("")}
-  
+  print word_chars
   reversed_chars = word_chars.collect {|word| word.reverse!}
 #  puts reversed_chars
   reversed_sentence = reversed_chars.join(" ")
