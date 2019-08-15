4f20ac183fa854aad4a9012c011aa041e9f32ade
John Plantamura <plantamurajk@gmail.com>
2019-08-15 13:58:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,19 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split( " ")
 #  print sentence_array
+
   word_chars = sentence_array.map {|word| word.split("")}
 #  print word_chars
+ 
   reversed_chars = word_chars.collect {|word| word.reverse!}
 #  print reversed_chars
+ 
   reversed_sentence_array = reversed_chars.map {|chars| chars.join("")}
 #  print reversed_sentence_array
+
   reversed_sentence = reversed_sentence_array.join(" ")
 #  print reversed_sentence
+ 
   return reversed_sentence
 end
 
