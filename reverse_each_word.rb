12eb0ea184d633e9395569fe3abbd597aa03fe5b
John Plantamura <plantamurajk@gmail.com>
2019-08-15 13:38:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence)
-  sentence_array = sentence.split
+  sentence_array = sentence.split( " ")
   reversed_array = sentence_array.reverse!
   reversed_sentence = reversed_array.join
   
