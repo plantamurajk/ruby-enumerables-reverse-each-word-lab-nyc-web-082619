38e65f05abdb3008d073cca562ff093f63a85d63
John Plantamura <plantamurajk@gmail.com>
2019-08-15 13:45:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split( " ")
-  puts sentence_array
+
   word_chars = sentence_array.collect {|word| word.split("")}
   
-  puts word_chars
+
   
   
 end
