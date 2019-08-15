da4413e84c983fba491b7957261dcaa26b4f56b0
John Plantamura <plantamurajk@gmail.com>
2019-08-15 13:57:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,14 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split( " ")
-  print sentence_array
+#  print sentence_array
   word_chars = sentence_array.map {|word| word.split("")}
-  print word_chars
+#  print word_chars
   reversed_chars = word_chars.collect {|word| word.reverse!}
-  print reversed_chars
+#  print reversed_chars
   reversed_sentence_array = reversed_chars.map {|chars| chars.join("")}
-  print reversed_sentence_array
+#  print reversed_sentence_array
   reversed_sentence = reversed_sentence_array.join(" ")
-  print reversed_sentence
+#  print reversed_sentence
   return reversed_sentence
 end
 
-reverse_each_word("Hello friend, how are you today?")
