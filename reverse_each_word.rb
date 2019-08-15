d0c0c8aad2b44d8a9ff10a1e90024ea601f50cc8
John Plantamura <plantamurajk@gmail.com>
2019-08-15 13:47:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,9 @@ def reverse_each_word(sentence)
   
   reversed_chars = word_chars.collect {|word| word.reverse!}
   
+  reversed_sentence = reversed_chars.join(" ")
   
+  return reversed_sentence
 end
 
 #reverse_each_word("Hello friend, how are you today?")
